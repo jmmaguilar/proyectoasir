61a4e5455c50c815a513bf5b5c83f01590e8e882
root <root@juanma-VirtualBox>
2019-05-03 13:13:39 +0200

    index 2, usuario, usuarioDAO, usuarioDAOImpl, conexion

--- webfct/src/main/java/es/cj/bean/Usuario.java
+++ webfct/src/main/java/es/cj/bean/Usuario.java
@@ -5,8 +5,9 @@ import java.io.Serializable;
 public class Usuario implements Serializable{
 	private int idUsuario;
 	private String login;
-	private String password;
 	private String nombre;
+	private String apellidos;
+	private String password;
 	private String email;
 	private int tipo;
 	
@@ -14,21 +15,23 @@ public class Usuario implements Serializable{
 		// TODO Auto-generated constructor stub
 	}
 
-	public Usuario(int idUsuario, String login, String password, String nombre, String email, int tipo) {
+	public Usuario(int idUsuario, String login, String nombre, String apellidos, String password, String email, int tipo) {
 		super();
 		this.idUsuario = idUsuario;
-		this.login = login;
-		this.password = password;
+		this.setLogin(login);
 		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.password = password;
 		this.email = email;
 		this.tipo = tipo;
 	}
 	
-	public Usuario(String login, String password, String nombre, String email, int tipo) {
+	public Usuario(String login, String nombre, String apellidos, String password, String email, int tipo) {
 		super();
-		this.login = login;
-		this.password = password;
+		this.setLogin(login);
 		this.nombre = nombre;
+		this.apellidos = apellidos;
+		this.password = password;
 		this.email = email;
 		this.tipo = tipo;
 	}
@@ -49,20 +52,28 @@ public class Usuario implements Serializable{
 		this.login = login;
 	}
 
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
 	public String getNombre() {
 		return nombre;
 	}
 
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
+	}
+
+	public String getApellidos() {
+		return apellidos;
+	}
+
+	public void setApellidos(String apellidos) {
+		this.apellidos = apellidos;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 	public String getEmail() {
@@ -83,9 +94,10 @@ public class Usuario implements Serializable{
 
 	@Override
 	public String toString() {
-		return "Usuario [idUsuario=" + idUsuario + ", login=" + login + ", password=" + password + ", nombre=" + nombre
-				+ ", email=" + email + ", tipo=" + tipo + "]";
+		return "Usuario [idUsuario=" + idUsuario + ", login=" + login + ", nombre=" + nombre + ", apellidos="
+				+ apellidos + ", password=" + password + ", email=" + email + ", tipo=" + tipo + "]";
 	}
+
 	
 	
 }
